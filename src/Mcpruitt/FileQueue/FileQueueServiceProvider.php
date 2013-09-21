776baddc08c0cdc6b28f9e635c1fa13acb00da7b
Michael Pruitt <mike@zoodia.com>
2013-09-21 00:02:07 -0400

    Initial commit.

---
+++ src/Mcpruitt/FileQueue/FileQueueServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Mcpruitt\FileQueue;
+
+use Illuminate\Support\ServiceProvider;
+
+class FileQueueServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
